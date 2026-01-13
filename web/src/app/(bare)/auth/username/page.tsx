5c471d1e65166c39fdf4f91bac45a20ce0fa6654
Prabhas <prabhas@MacBook-Pro.local>
2026-01-13 22:50:58 +0530

    remove username screen and their crosponding screen

--- web/src/app/(bare)/auth/username/page.tsx
+++
@@ -1,5 +0,0 @@
-import { UsernameSelectionScreen } from "@/screens/auth/UsernameSelectionScreen/UsernameSelectionScreen";
-
-export default function Page() {
-  return <UsernameSelectionScreen />;
-}
