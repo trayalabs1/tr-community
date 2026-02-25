9be6d04ddc8b7741678e7db43f8c0d48bb552236
PrabhakarTraya <prabhakarmandal@traya.health>
2026-02-25 15:49:55 +0530

    fixing profile username

--- web/src/app/(dashboard)/m/[handle]/page.tsx
+++ web/src/app/(dashboard)/m/[handle]/page.tsx
@@ -10,28 +10,18 @@ type Props = {
 };
 
 export default async function Page(props: Props) {
+  const params = await props.params;
+  const { handle } = params;
+
+  if (handle.startsWith("temp_")) {
+    return <TempHandlePrompt />;
+  }
+
   try {
-    const params = await props.params;
-    const { handle } = params;
-
-    const isTempHandle = handle.startsWith("temp_");
-
-    if (isTempHandle) {
-      const session = await getServerSession();
-      return <TempHandlePrompt initialName={session?.name} />;
-    }
-
     const session = await getServerSession();
     const { data } = await profileGet(handle);
     return <ProfileScreen initialSession={session} profile={data} />;
   } catch (e) {
-    const params = await props.params;
-    const { handle } = params;
-
-    if (handle.startsWith("temp_")) {
-      return <TempHandlePrompt />;
-    }
-
     return <UnreadyBanner error="Something went wrong. Please try again later." />;
   }
 }
