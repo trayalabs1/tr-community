c424ae0d216205a7c8466843067e1442c1a3fb3e
Hema-sagar-07 <hemasagarb@traya.health.com>
2026-04-09 14:13:20 +0530

    stopped rehydrator

--- app/services/sentiment/sentiment.go
+++ app/services/sentiment/sentiment.go
@@ -4,7 +4,7 @@ import (
 	"go.uber.org/fx"
 
 	"github.com/Southclaws/storyden/app/services/sentiment/ranker"
-	"github.com/Southclaws/storyden/app/services/sentiment/rehydrator"
+	// "github.com/Southclaws/storyden/app/services/sentiment/rehydrator"
 	"github.com/Southclaws/storyden/app/services/sentiment/sentiment_job"
 )
 
@@ -12,6 +12,6 @@ func Build() fx.Option {
 	return fx.Options(
 		fx.Provide(ranker.New),
 		sentiment_job.Build(),
-		rehydrator.Build(),
+		// rehydrator.Build(),
 	)
 }
