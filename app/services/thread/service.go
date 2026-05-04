8cb992cc8d64efefc4be84465f2e301abcd5edb6
Hema-sagar-07 <hemasagarb@traya.health.com>
2026-05-04 17:32:09 +0530

    rank assignment

--- app/services/thread/service.go
+++ app/services/thread/service.go
@@ -27,6 +27,7 @@ import (
 	"github.com/Southclaws/storyden/app/services/moderation"
 	"github.com/Southclaws/storyden/app/services/report/system_report"
 	"github.com/Southclaws/storyden/app/services/semdex"
+	"github.com/Southclaws/storyden/internal/ent"
 	"github.com/Southclaws/storyden/internal/infrastructure/instrumentation/spanner"
 	"github.com/Southclaws/storyden/internal/infrastructure/pubsub"
 )
@@ -95,6 +96,7 @@ func Build() fx.Option {
 type service struct {
 	ins spanner.Instrumentation
 
+	db             *ent.Client
 	accountQuery   *account_querier.Querier
 	threadQuerier  *thread_querier.Querier
 	threadWriter   *thread_writer.Writer
@@ -111,6 +113,7 @@ type service struct {
 func New(
 	ins spanner.Builder,
 
+	db *ent.Client,
 	accountQuery *account_querier.Querier,
 	threadQuerier *thread_querier.Querier,
 	threadWriter *thread_writer.Writer,
@@ -126,6 +129,7 @@ func New(
 	return &service{
 		ins: ins.Build(),
 
+		db:             db,
 		accountQuery:   accountQuery,
 		threadQuerier:  threadQuerier,
 		threadWriter:   threadWriter,
