b5ccc8ca44b83d3af8df9b7754b08892445361b3
Hema-sagar-07 <hemasagarb@traya.health.com>
2026-05-12 18:18:40 +0530

    bah content change

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -7,7 +7,7 @@
 export const STREAK_IMAGES: Record<number, string> = {
   3: "https://cdn.shopify.com/s/files/1/0100/1622/7394/files/3_days_streak.png?v=1775818613",
   7: "https://cdn.shopify.com/s/files/1/0100/1622/7394/files/7_days_streak.png?v=1775818613",
-  21: "https://cdn.shopify.com/s/files/1/0100/1622/7394/files/21_days_streak.png?v=1775818613",
+  21: "https://cdn.shopify.com/s/files/1/0100/1622/7394/files/21_days_streak_1.jpg?v=1778580737",
 };
 
 export const DEFAULT_STREAK_IMAGE_KEY = 3;
